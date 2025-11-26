c6908bffcfb64cb9ea05806eee7775fafc70b7f5
Semernikov Nikita <niksemlearn@gmail.com>
2025-11-26 19:25:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', function(event) {
+document.addEventListener('DOMContentLoaded', function() {
     let currentRadioValue = "";
     function calculateTotal() {
         const countInput = document.getElementById('count');
